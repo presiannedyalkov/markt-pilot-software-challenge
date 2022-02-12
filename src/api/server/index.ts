6b28b5e5a607e6d87d005dda6aa45fb299489ff6
Presian Nedyalkov <presian.nedyalkov@gmail.com>
2022-02-13 00:58:29 +0100

    fix(detail): fix dynamic route matching

--- src/api/server/index.ts
+++ src/api/server/index.ts
@@ -6,12 +6,15 @@ export default {
         return axios
             .get('https://server-ip/chocolates')
             .then(response => response.data)
-            .catch(() => {
-                // the request is expected to land here for production build
-                // get the production mock data
-                return axios.get('chocolate-data.json')
-                    .then(response => response.data)
-                    .catch(error => console.log(error))
+            .catch((error) => {
+                if (process.env.NODE_ENV === 'development' && process.env.VUE_APP_API_CLIENT === 'server') {
+                    // the request is expected to land here for production build
+                    // get the production mock data
+                    return axios.get('chocolate-data.json')
+                        .then(response => response.data)
+                        .catch(error => console.log(error))
+                }
+                return error;
             });
     }
 }
