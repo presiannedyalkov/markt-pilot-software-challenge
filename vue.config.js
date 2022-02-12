304a6a639ce445fe69fff7996afc938ab5450b67
Presian Nedyalkov <presian.nedyalkov@gmail.com>
2022-02-12 15:30:45 +0100

    fix(config): change public path for gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    publicPath: "/markt-pilot-software-challenge/",
     chainWebpack: config => {
         if (process.env.NODE_ENV === 'development' && process.env.VUE_APP_API_CLIENT === 'server') {
             config
